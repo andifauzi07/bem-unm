077d2abd3a8dee045e4a6f359c97a830ab336962
andi fauzi <andifauzi0709@gmail.com>
2025-04-11 00:24:00 +0800

    edit bank data

--- app/tentang-kami/bank-data/page.jsx
+++ app/tentang-kami/bank-data/page.jsx
@@ -5,7 +5,7 @@ export default function BankData() {
 	return (
 		<main className="h-screen lg:py-28 py-24 text-center">
 			<h1 className="lg:text-5xl text-2xl font-bold pt-4 lg:pt-0 lg:pb-4">
-				Bank <span className="text-[#E15701]">Data</span>
+				<span className="text-[#E15701]">Laporan</span> Pertanggung Jawaban
 			</h1>
 			<p className="text-[#8A8888] lg:text-2xl pb-6 lg:pb-8">Laporan Pertanggung Jawaban Kepengurusan</p>
 			<div className="w-full flex justify-center">
